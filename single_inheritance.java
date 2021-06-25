c4dc472ddf3936ff64045a775cb00ec7d9827486
Yugam Shukla <42417007+YugamShukla@users.noreply.github.com>
2021-06-25 13:55:19 +0530

    Update single_inheritance.java

--- single_inheritance.java
+++ single_inheritance.java
@@ -21,3 +21,20 @@ class Main {
 
 //Output:-
 //1100
+
+class Salary {
+  int base=1000;
+}
+class Bonus extends Salary {
+    int extra=100;
+}
+class Main {
+  public static void main(String args[]) {
+    Bonus b = new Bonus();
+    int total_salary = b.base+ b.extra;
+    System.out.println(total_salary);
+  }
+}
+
+//Output:-
+//1100
