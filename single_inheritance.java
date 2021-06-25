fb64aefdbc49c21e92d298e1deba5dbb5fa23245
Yugam Shukla <42417007+YugamShukla@users.noreply.github.com>
2021-06-25 13:49:36 +0530

    Create single_inheritance.java

---
+++ single_inheritance.java
@@ -0,0 +1,20 @@
+class Salary {
+  int base() {
+    return 1000;
+  }
+}
+class Bonus extends Salary {
+  int extra() {
+    return 100;
+  }
+}
+class Main {
+  public static void main(String args[]) {
+    Bonus b = new Bonus();
+    int total_salary = b.base() + b.extra();
+    System.out.println(total_salary);
+  }
+}
+
+//Output:-
+//1100
